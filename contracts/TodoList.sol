7f20f9f0b42dcafd9592f487f01a2e912698224c
Brymer <meneses.brymer@gmail.com>
2022-09-11 03:31:41 +0800

    improve smart contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,42 +4,69 @@ pragma solidity ^0.8.0;
 contract TodoList {
 
     struct Task {
-        uint id;
         string content;
         bool isCompleted;
     }
 
-    struct User {
-        uint totalTasks;
-        Task[] tasks;
-    }
-
     event TaskCreated(address user, uint taskId);
-    event TaskFinished(address user, uint taskId);
+    event TasksUpdated(address user);
 
-
-    mapping(address => User) users;
-
-    function createTask(string memory content) public {
-        User storage user = users[msg.sender];
-        uint taskId = user.totalTasks;
-
-        user.tasks.push( Task(taskId, content, false) ); 
-
-        emit TaskCreated(msg.sender, taskId);
-        user.totalTasks++;
-
-    }
-
-    function finishTask(uint taskId) public {
-        Task storage task = users[msg.sender].tasks[taskId];
-
-        task.isCompleted = true;
-        emit TaskFinished(msg.sender, taskId);
-    }
+    mapping(address => Task[]) database;
 
     function retrieveTasks() public view returns( Task[] memory ) {
-        return users[msg.sender].tasks;
+      return database[msg.sender];
+    }
+
+    function createTask(string memory content) public {
+      Task[] storage tasks = database[msg.sender];
+      tasks.push(Task(content, false));
+
+      emit TaskCreated(msg.sender, tasks.length - 1);
+    }
+
+    function finishTask(uint id) public {
+      Task storage task = database[msg.sender][id];
+      task.isCompleted = true;
+    }
+
+    function swapTasks(uint id1, uint id2) public {
+      Task memory temp = database[msg.sender][id1];
+      database[msg.sender][id1] = database[msg.sender][id2];
+      database[msg.sender][id2] = temp;
+    }
+
+    enum Operation {
+      CREATE,
+      DELETE,
+      SWAP,
+      FINISH
+    }
+
+    struct TaskOperation {
+      Operation op;
+      uint taskId;
+      uint swap;
+      string content;
+    }
+
+    // Helper function to bundle transactions
+    function updateTasks(TaskOperation[] memory ops) public {
+
+      for (uint i=0; i<ops.length; i++) {
+          TaskOperation memory op = ops[i];
+
+          if (op.op == Operation.CREATE) {
+            createTask(op.content);
+          } else if (op.op == Operation.DELETE) {
+            delete database[msg.sender][op.taskId];
+          } else if (op.op == Operation.SWAP) {
+            swapTasks(op.taskId, op.swap);
+          } else if (ops[i].op == Operation.FINISH) {
+            finishTask(op.taskId);
+          }
+      }
+      emit TasksUpdated(msg.sender);
+
     }
 
 
